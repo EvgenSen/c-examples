514983c2b4043445def70c2b5ef55cdc3db64e36
EvgenSen <sennikov-work@ya.ru>
2019-02-06 17:06:59 +0700

    time_of_work: Added new functions

--- time_of_work/time_of_work.c
+++ time_of_work/time_of_work.c
@@ -14,24 +14,43 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>   // for struct timeval
-#include <time.h>       // for clock_t
+#include <time.h>       // for clock_t, time_t, struct timespec
 
-void print_time_diff (struct timeval tv_start, struct timeval tv_end)
+#define TEST_CLOCK_ID CLOCK_MONOTONIC // May be: CLOCK_REALTIME, CLOCK_PROCESS_CPUTIME_ID, CLOCK_MONOTONIC or get id by clock_getcpuclockid()
+
+void print_time_diff_timeval (struct timeval tv_start, struct timeval tv_end)
 {
     int ms = (int)(tv_end.tv_usec - tv_start.tv_usec) / 1000;
     int s  = (int)(tv_end.tv_sec - tv_start.tv_sec);
     if (ms < 0)
         ms = 1 - ms;
 
-    printf("Spent time 1: %d ms\n", s * 1000 + ms);
+    printf("Spent time 1 (timeval):  %d ms\n", s * 1000 + ms);
 }
 
-int main(int argc, char *argv[])
+void print_time_diff_timespec (struct timespec st, struct timespec end)
 {
-    struct timeval start_time, end_time;
+    // Time diff in ns:
+    unsigned long long delta_timespec = 1000000000ULL * (end.tv_sec - st.tv_sec) + (end.tv_nsec - st.tv_nsec);
+    unsigned long ms = (delta_timespec) / 1000000;
+    unsigned long ap = (delta_timespec) % 1000000;
 
-    gettimeofday(&start_time, 0);
-    clock_t start_time_clock = clock();
+    //printf("Spent time 2 (timespec): %llu ns\n", delta_timespec);
+    printf("Spent time 2 (timespec): %lu.%lu ms\n", ms, ap);
+}
+
+int main (int argc, char *argv[])
+{
+    struct timeval st_timeval, end_timeval;
+    clock_t st_clock, end_clock;
+    time_t st_time, end_time;
+    struct timespec st_timespec, end_timespec;
+
+    // Check start time
+    gettimeofday (&st_timeval, 0);
+    st_clock = clock();
+    st_time  = time(NULL);
+    clock_gettime (TEST_CLOCK_ID, &st_timespec);
 
 #if 0
     // sleep don't work with clock
@@ -40,16 +59,23 @@ int main(int argc, char *argv[])
 #else
     int i = 0, j = 0;
     for (i = 0; i < 1000000; i++)
-        for (j = 0; j < 1000; j++)
+        for (j = 0; j < 2900; j++)
             j = j; // do nothing
 #endif
 
-    gettimeofday(&end_time, 0);
-    clock_t end_time_clock = clock();
+    // Check end time
+    gettimeofday (&end_timeval, 0);
+    end_clock = clock();
+    end_time  = time(NULL);
+    clock_gettime (TEST_CLOCK_ID, &end_timespec);
 
-    print_time_diff(start_time, end_time);
+    // Print result
+    print_time_diff_timeval (st_timeval, end_timeval);
+    print_time_diff_timespec (st_timespec, end_timespec);
 
-    double time_spent = (double)(end_time_clock - start_time_clock) / CLOCKS_PER_SEC;
-    printf("Spent time 2: %f s\n", time_spent);
+    double delta_clock = (double)(end_clock - st_clock) / CLOCKS_PER_SEC;
+    double delta_time  = difftime (end_time, st_time);
+    printf("Spent time 3 (clock):    %f ms\n", delta_clock*1000);
+    printf("Spent time 4 (time):     %f ms\n", delta_time*1000);
     return 0;
 }
