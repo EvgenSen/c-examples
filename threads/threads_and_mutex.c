aa2b5afc70ae9ce73be3dd6c3ec50e74ed03c675
EvgenSen <sennikov-work@ya.ru>
2022-08-27 13:00:31 +0700

    threads: Small refactoring, add callback function

--- threads/threads_and_mutex.c
+++ threads/threads_and_mutex.c
@@ -21,79 +21,91 @@
 #include <sys/syscall.h>
 
 #define THREAD_COUNT 10  // Общее количество потоков (рабочих добывающих золото)
-#define ZOLOTO_ONE   20  // Количество золота, добываемое за раз
-#define ZOLOTO_ALL   600 // Общее количество золота
+#define GOLD_ONE     20  // Количество золота, добываемое за раз
+#define GOLD_ALL     600 // Общее количество золота
 
-static int             zoloto;
+typedef int cb_func(int local_id, int gold_one);
+
+struct thread_data
+{
+	int      local_id;
+	cb_func *get_gold_cb;
+};
+
+static int             gold;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/** Get `gold_one` from `gold`
+ *
+ * @param    local_id      - thread ID
+ *           gold_one      - amount of gold getting at a time
+ *
+ * @return   0  - gold getted successfully
+ *          -1  - gold is over
+ */
+int get_gold(int local_id, int gold_one)
+{
+	int sleep_us = 0;
+	int ret      = -1;
+
+	pthread_mutex_lock(&mutex);
+
+	if (gold > 0)
+	{
+		gold -= gold_one;
+		if (gold < 0)
+			gold = 0;
+		// На добычу золота юнит тратит от 20 до 40 мс
+		sleep_us = rand() % 20000 + 20000;
+		usleep(sleep_us);
+		printf("[pid = %d, tid = %ld] %2d Юнит забрал золото, потратив %d мкс. В шахте осталось: %d\n", getpid(),
+		       syscall(__NR_gettid), local_id + 1, sleep_us, gold);
+		ret = 0;
+	}
+
+	pthread_mutex_unlock(&mutex);
+
+	return ret;
+}
 
 void thread_main(void *arg)
 {
 	// преобразуем входные аргументы
-	int  local_id        = *(int *)arg;
-	int  sleep_us        = 0;
-	char thread_name[32] = {0};
+	struct thread_data *data     = (struct thread_data *)arg;
+	int                 sleep_us = 0;
 
-	sprintf(thread_name, "Unit %d", local_id);
-	// Задаем имя текущего потока
-	prctl(PR_SET_NAME, thread_name, 0, 0, 0);
-
-	srand(time(NULL) + local_id * 2);
+	srand(time(NULL) + data->local_id * 2);
 
 	while (1)
 	{
-		pthread_mutex_lock(&mutex);
-
-		if (zoloto > 0)
-		{
-			zoloto -= ZOLOTO_ONE;
-			// На добычу золота юнит тратит от 20 до 40 мс
-			sleep_us = rand() % 20000 + 20000;
-			usleep(sleep_us);
-			printf("[pid = %d, tid = %ld] %2d Юнит забрал золото, потратив %d мкс. В шахте осталось: %d\n", getpid(),
-			       syscall(__NR_gettid), local_id + 1, sleep_us, zoloto);
-		}
-		else
-		{
-			pthread_mutex_unlock(&mutex);
+		if (data->get_gold_cb(data->local_id, GOLD_ONE) < 0)
 			break;
-		}
-
-		pthread_mutex_unlock(&mutex);
 
 		sleep_us = rand() % 90000 + 10000;
 		// printf("%2d sleep_us = %d\n", local_id + 1, sleep);
 		usleep(sleep_us);
 	}
-
-	sleep(100); // чтобы поток дольше висел и можно было посмотреть в списке процессов
 }
 
 int main(int argc, char **argv)
 {
-	int       i;
-	int       id[THREAD_COUNT] = {0};
-	pthread_t thread_id[THREAD_COUNT];
+	int                i;
+	pthread_t          thread_id[THREAD_COUNT];
+	struct thread_data data[THREAD_COUNT];
 
-	zoloto = ZOLOTO_ALL;
-	printf("Началась добыча золота. Золота в шахте: %d\n", zoloto);
+	gold = GOLD_ALL;
+	printf("Началась добыча золота. Золота в шахте: %d\n", gold);
 
 	for (i = 0; i < THREAD_COUNT; i++)
 	{
-		id[i] = i;
-		// создаем потоки и передаем его ID в качестве аргумента
-		if (pthread_create(&thread_id[i], NULL, (void *)&thread_main, &id[i]) != 0)
+		data[i].get_gold_cb = get_gold;
+		data[i].local_id    = i;
+		// создаем потоки и передаем структуру с ID и функцией поулчения золота в качестве аргумента
+		if (pthread_create(&thread_id[i], NULL, (void *)&thread_main, &data[i]) != 0)
 		{
 			perror("Could not create pthread");
 			continue;
 		}
-#if 0
-		// задаем имя дочернего потока из родительского
-		if ( pthread_setname_np(thread_id[i], "Unit X") != 0 )
-		{
-			perror("Could not set pthread name");
-		}
-#endif
 	}
 
 	for (i = 0; i < THREAD_COUNT; i++)
@@ -103,7 +115,7 @@ int main(int argc, char **argv)
 	}
 
 	pthread_mutex_destroy(&mutex);
-	printf("Золота в шахте: %d\n", zoloto);
+	printf("Золота в шахте: %d\n", gold);
 
 	return 0;
 }
