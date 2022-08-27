b0161b9b228f9c3c85756df00909b3b65cc1f63b
EvgenSen <sennikov-work@ya.ru>
2022-08-27 13:44:40 +0700

    debug: Add backtrace example

---
+++ debug/README.md
@@ -0,0 +1,31 @@
+## debug
+
+### backtrace
+
+Output example:
+
+<pre>
+$ ./backtrace
+Started
+
+Obtained 4 stack frames.
+./backtrace(print_backtrace+0x2c) [0x5651a4290235]
+./backtrace(main+0x24) [0x5651a429038b]
+/lib/x86_64-linux-gnu/libc.so.6(__libc_start_main+0xf3) [0x7f321141d0b3]
+./backtrace(_start+0x2e) [0x5651a429014e]
+
+foo: depth = 0
+
+Obtained 10 stack frames.
+./backtrace(print_backtrace+0x2c) [0x5651a4290235]
+./backtrace(foo+0x47) [0x5651a4290312]
+./backtrace(bar+0x23) [0x5651a429033c]
+./backtrace(foo+0x23) [0x5651a42902ee]
+./backtrace(bar+0x23) [0x5651a429033c]
+./backtrace(foo+0x23) [0x5651a42902ee]
+./backtrace(bar+0x23) [0x5651a429033c]
+./backtrace(foo+0x23) [0x5651a42902ee]
+./backtrace(main+0x2e) [0x5651a4290395]
+/lib/x86_64-linux-gnu/libc.so.6(__libc_start_main+0xf3) [0x7f321141d0b3]
+
+</pre>
---
+++ debug/backtrace.c
@@ -0,0 +1,67 @@
+/*
+ * backtrace.c - backtrace example
+ *
+ * Author: Evgeniy Sennikov <sennikov.work@ya.ru>
+ *
+ * Licence: GPLv3
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <execinfo.h>
+
+int foo(int depth);
+int bar(int depth);
+
+/* https://www.gnu.org/software/libc/manual/html_node/Backtraces.html */
+void print_backtrace (void)
+{
+	void *array[10];
+	char **strings;
+	int size, i;
+
+	size = backtrace (array, 10);
+	strings = backtrace_symbols (array, size);
+	if (strings != NULL)
+	{
+
+	printf ("\nObtained %d stack frames.\n", size);
+	for (i = 0; i < size; i++)
+		printf ("%s\n", strings[i]);
+	}
+	printf ("\n");
+
+	free (strings);
+}
+
+int foo(int depth)
+{
+	if(depth > 0)
+		return bar(--depth);
+
+	printf("%s: depth = %d\n", __FUNCTION__, depth);
+	print_backtrace();
+	return 0;
+}
+
+int bar(int depth)
+{
+	if(depth > 0)
+		return foo(--depth);
+
+	printf("%s: depth = %d\n", __FUNCTION__, depth);
+	print_backtrace();
+	return 0;
+}
+
+int main()
+{
+	int depth = 6;
+
+	printf("Started\n");
+	print_backtrace();
+
+	foo(depth);
+
+	return 0;
+}
