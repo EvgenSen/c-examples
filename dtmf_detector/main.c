d260eee39c6b219362f21b25f67765fd3f1d61bf
EvgenSen <sennikov-work@ya.ru>
2024-04-15 18:15:23 +0700

    dtmf_detecot: Split main function, create new file without signal

--- dtmf_detector/main.c
+++ dtmf_detector/main.c
@@ -1,7 +1,8 @@
 /**
  * main.c - wrapper over dsp.c and wave.c
  *
- * This example open WAV file, read data and find DTMF signals
+ * This example open WAV file, read data, find DTMF signals,
+ * remove it and save data to new file.
  *
  * Author: Evgeniy Sennikov <sennikov.work@ya.ru>
  *
@@ -19,13 +20,89 @@
 #include "wave.h"
 #include "dsp.h"
 
-int main(int argc, char **argv)
+#define OUTPUT_FILENAME "out.wav"
+
+/* Read file with 1 channel 16 bit data
+   Input arg:  filename  - WAV file name
+   Output arg: hdr       - header of WAV file
+               wave_data - data array
+   Return:     0 - Success, otherwise - error */
+int read_wave_file(char *filename, struct wave_header *hdr, int16_t **wave_data)
 {
 	FILE *fd;
+
+	if ((fd = fopen(filename, "rb")) == NULL)
+	{
+		printf("Error: Can't open file %s (%s)\n", filename, strerror(errno));
+		return -1;
+	}
+
+	fread(hdr, sizeof(struct wave_header), 1, fd);
+
+	if(wave_check_header(*hdr) < 0)
+	{
+		printf("Error: Invalid file header\n");
+		fclose(fd);
+		return -3;
+	}
+
+	wave_print_header_info(*hdr);
+
+	*wave_data = wave_read_data(*hdr, fd);
+	if (*wave_data == NULL)
+	{
+		printf("Error: Can't read file %s\n", filename);
+		fclose(fd);
+		return -3;
+	}
+
+	fclose(fd);
+
+	return 0;
+}
+
+/* Write to WAV file 1 channel 16 bit data
+   Input arg:  filename  - WAV file name
+               hdr       - header of WAV file
+               wave_data - data array
+   Return:     0 - Success, otherwise - error */
+int write_wave_file(char *filename, struct wave_header *hdr, int16_t *wave_data)
+{
+	FILE *fd;
+	int i;
+	int16_t * wave_data_ptr = NULL;
+	long num_samples = (8 * hdr->data_hdr.len) / (hdr->fmt_hdr.nchan * hdr->fmt_hdr.bits_per_sample);
+
+	if ((fd = fopen(filename, "wb")) == NULL)
+	{
+		printf("Error: Can't create file %s (%s)\n", filename, strerror(errno));
+		return -1;
+	}
+
+	fwrite((void*)hdr, sizeof(struct wave_header), 1, fd);
+
+	wave_data_ptr = wave_data;
+	for (i = 1; i <= num_samples; i++)
+	{
+		char data_buffer[2];
+		data_buffer[0] = wave_data_ptr[i] & 0x00ff;
+		data_buffer[1] = wave_data_ptr[i] >> 8;
+
+		fwrite((void*)&data_buffer, sizeof(data_buffer), 1, fd);
+	}
+
+	fclose(fd);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
 	struct wave_header hdr;
+	int16_t * wave_data = NULL;
 	digit_detect_state_t dtmf_detect_state;
 	int last_num_digit = 0;
-	int i;
+	int i, j, ret;
 
 	memset(&dtmf_detect_state, 0, sizeof(digit_detect_state_t));
 
@@ -35,32 +112,8 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	if ((fd = fopen(argv[1], "rb")) == NULL)
-	{
-		printf("Error: Can't open file %s (%s)\n", argv[1], strerror(errno));
-		return -2;
-	}
-
-	fread((void*)&hdr, sizeof(hdr), 1, fd);
-
-	if(wave_check_header(hdr) < 0)
-	{
-		printf("Error: Invalid file header\n");
-		fclose(fd);
-		return -3;
-	}
-
-	wave_print_header_info(hdr);
-
-	int16_t * wave_data = wave_read_data(hdr, fd);
-	if (wave_data == NULL)
-	{
-		printf("Error: Can't read file %s\n", argv[1]);
-		fclose(fd);
-		return -3;
-	}
-
-	fclose(fd);
+	if ((ret = read_wave_file(argv[1], &hdr, &wave_data)) < 0)
+		return ret;
 
 #if 0
 	{
@@ -101,14 +154,20 @@ int main(int argc, char **argv)
 				         dtmf_detect_state.digits[last_num_digit - 1]);
 
 			}
-			// printf("%s: ret:%d\n", __FUNCTION__, ret);
-			// return PJ_EINVALIDOP;
+			/* reset the detected signal */
+			for (j = 0; j < 160; j++)
+			{
+				wave_data_ptr[j] = 0;
+			}
 		}
 
 		if((i+1)*160 > num_samples)
 			break;
 		wave_data_ptr += 160;
 	}
+
+	if ((ret = write_wave_file(OUTPUT_FILENAME, &hdr, wave_data)) < 0)
+		return ret;
 
 	free(wave_data);
 
